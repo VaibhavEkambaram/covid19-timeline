548737e8b8b763bc2ef422d9eb3eb2d08b5cd5d0
Vaibhav Ekambaram <vekambaram00@gmail.com>
2023-04-01 11:31:35 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   headers: [
     {
-      "source": "/api/(.*)",
+      "source": "/(.*)",
       "headers": [
         { "key": "Access-Control-Allow-Credentials", "value": "true" },
         { "key": "Access-Control-Allow-Origin", "value": "*" },
